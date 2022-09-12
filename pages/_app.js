62ad60d4bdeabf0f7d754ebbc5115c593f546e3b
Phai1986 <watcharin@digitiv.net>
2022-09-12 12:08:42 +0700

    test12092022

--- pages/_app.js
+++ pages/_app.js
@@ -23,6 +23,7 @@ const MyApp = ({ Component, pageProps }) => {
             <script id="cookieWow" type="text/javascript" src="https://cookiecdn.com/configs/ySqETpPb2KtbQYgDdpJk2ujd" data-cwcid="ySqETpPb2KtbQYgDdpJk2ujd"></script>
             <Favicon url="/images/JD_Food.svg"></Favicon>
             <Component {...pageProps} />
+            {/* <p>test 12092022</p> */}
         </>
     )
 
